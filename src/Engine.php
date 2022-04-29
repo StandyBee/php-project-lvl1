bc75dc6fcb11565a19be904b1d839996684d8dc2
Anton <afisak94@gmail.com>
2022-04-29 05:22:28 +0300

    add src/ new structure of game even

---
+++ src/Engine.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Project\Engine;
+
+use function cli\line;
+use function cli\prompt;
+
+const ROUNDS_COUNT = 3;
+function startGame($task, $answerPair)
+{
+    line('Welcome to the Brain Games!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+    line($task);
+    for ($i = 1; $i <= ROUNDS_COUNT; $i++) {
+    $question = $answerPair[0];
+    $correctAnswer = $answerPair[1];
+    line('Question: ' . $question[$i]);
+    $answer = prompt('Your answer');
+    if ($correctAnswer[$i] == $answer) {
+	line('Correct!');
+    } else {
+	line("'{$answer}' is wrong answer ;(. Correct answer was '{$correctAnswer[$i]}'");
+	line("Let's try again, $name!");
+	return;
+      }
+    }
+    line("Congratulations, $name!");
+}
---
+++ src/Games/even-game.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Project\Games\even;
+
+use function Project\Engine\startGame;
+
+use const Project\Engine\ROUNDS_COUNT;
+
+const GAME_TASK = ('Answer "yes" if the number is even, otherwise answer "no".');
+function runGame()
+{
+    $correctAnswer = [];
+    $question = [];
+    for ($i = 0; $i <= ROUNDS_COUNT; $i++) {
+	$num = rand(0, 99);
+	$question[] = $num;
+	$correctAnswer[] = isEven($num) ? 'yes' : 'no';
+    }
+    $answerPair = [$question, $correctAnswer];
+    return (startGame(GAME_TASK, $answerPair));
+}
+
+function isEven($num)
+{
+    if ($num % 2 === 0) {
+	return true;
+    }
+	return false;
+}
