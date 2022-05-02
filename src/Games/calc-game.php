0d74e938076d098127480cce884219fd1790fa2e
Anton <afisak94@gmail.com>
2022-05-02 17:12:25 +0300

    add files for logic of calc game

---
+++ src/Games/calc-game.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Project\Games\calc;
+
+use function Project\Engine\startGame;
+
+use const Project\Engine\ROUNDS_COUNT;
+
+const GAME_TASK = ('What is the result of the expression?');
+function runGame()
+{
+    $correctAnswer = [];
+    $question = [];
+    for ($i = 0; $i <= ROUNDS_COUNT; $i++) {
+	$sign = ['+', '-', '*'];
+	$j = rand(0, 2);
+	$randomSign = $sign[$j];
+	$num = rand(0, 99);
+	$num2 = rand(0, 99);
+	$question[] = $num . $randomSign . $num2;
+	$correctAnswer[] = calculate($num, $num2, $randomSign);
+	
+    }
+    $answerPair = [$question, $correctAnswer];
+    return (startGame(GAME_TASK, $answerPair));
+}
+function calculate($num, $num2, $randomSign)
+{
+    if ($randomSign == '+') {
+	return ($num + $num2);    
+    } elseif ($randomSign == '-') {
+	return ($num - $num2);    
+    } elseif ($randomSign == '*') {
+	return ($num * $num2);
+    }
+}
+
