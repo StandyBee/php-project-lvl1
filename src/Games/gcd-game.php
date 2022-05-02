8ead41a7e8882353a13c897b612e0156acf1a897
Anton <afisak94@gmail.com>
2022-05-02 17:53:01 +0300

    add files for logic of gcd game

---
+++ src/Games/gcd-game.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Project\Games\gcd;
+
+use function Project\Engine\startGame;
+
+use const Project\Engine\ROUNDS_COUNT;
+
+const GAME_TASK = ('Find the greatest common divisor of given numbers.');
+function runGame()
+{
+    $correctAnswer = [];
+    $question = [];
+    for ($i = 0; $i <= ROUNDS_COUNT; $i++) {
+	$num = rand(0, 99);
+	$num2 = rand(0, 99);
+	$question[] = "$num $num2";
+	$result = findGcd($num, $num2);
+	$correctAnswer[] = $result;
+    }
+	$answerPair = [$question, $correctAnswer];
+	     return (startGame(GAME_TASK, $answerPair));
+}
+function findGcd($num, $num2)
+{
+    if ($num == 0 || $num2 == 0) {
+	return abs( max(abs($num), abs($num2)));
+    } else {    
+	$result = $num % $num2;
+	return ($result != 0) ? findGcd($num2, $result) : abs($num2);
+    }
+}
