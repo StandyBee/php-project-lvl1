6c5a4bf4c61f25ecec27ad42a5ed256b5e2344fb
Anton <afisak94@gmail.com>
2022-05-02 18:06:28 +0300

    add files for logic of prime game

---
+++ src/Games/prime-game.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Project\Games\prime;
+
+use function Project\Engine\startGame;
+
+use const Project\Engine\ROUNDS_COUNT;
+
+const GAME_TASK = ('Answer "yes" if the number is prime, otherwise answer "no".');
+function runGame()
+{
+    $correctAnswer = [];
+    $question = [];
+    for ($i = 0; $i <= ROUNDS_COUNT; $i++) {
+	$num = rand(0, 199);
+	$question[] = $num;
+	$correctAnswer[] = isPrime($num) ? 'yes' : 'no';
+    }
+    $answerPair = [$question, $correctAnswer];
+    return (startGame(GAME_TASK, $answerPair));
+}
+function isPrime($num)
+{
+    if ($num == 0) {
+	return false;
+    }
+    for ($i = 2; $i < $num; $i++) {
+	if ($num % $i == 0) {
+	    return false;
+	}
+    }
+    return true;
+}
